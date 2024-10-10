79b5584b0253b260b7933bce27850387b8a4f472
Jhony Renteria <jhonyrenteria@gmail.com>
2024-10-09 23:12:49 -0500

    Prototype news page
    add prototype page newa

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "10f3368cb30d209947fa9d7d630c0dff",
+const RESOURCES = {"flutter_bootstrap.js": "da57a04deb13b9b408ba54153234c47a",
 "version.json": "4a94c7dd139eb4e4c416b934820b7844",
 "index.html": "b836f73b3b39f116eb4d48f18df14f90",
 "/": "b836f73b3b39f116eb4d48f18df14f90",
 "styles.css": "3fa077216edf91518eeb280fcb408737",
-"main.dart.js": "fffd934d272dab4df860ffde0d127c1b",
+"main.dart.js": "4b4fb4f8248a45308ae1dd6f06a4dc49",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
