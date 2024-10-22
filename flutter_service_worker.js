916f5c6698649f135c0fceb40f49380abf1c212a
Jhony Renteria Rodriguez <jhonyrenteria@gmail.com>
2024-10-22 05:46:01 +0000

    Deploy website update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a659c6e0d1e94f776efe219d74a14ddc",
+const RESOURCES = {"flutter_bootstrap.js": "4c53c7c961342ae78268d80279520e44",
 "version.json": "4a94c7dd139eb4e4c416b934820b7844",
 "index.html": "b836f73b3b39f116eb4d48f18df14f90",
 "/": "b836f73b3b39f116eb4d48f18df14f90",
 "styles.css": "a87ad9f5c3c5b3e28c9891347351f349",
-"main.dart.js": "74861327e899e887395ab87e98c4e733",
+"main.dart.js": "38e040f3917f8809b786ea4417567d7a",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
